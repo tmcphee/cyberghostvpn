7a8726307d906fee2d0c7a87f61d435a99329784
Tyler McPhee <32398415+tmcphee@users.noreply.github.com>
2023-03-07 20:49:33 -0500

    Added country and args variables. New DNS
    Added the ability to customize the COUNTRY and ARGS though variables.
    Added Smart DNS Selection to the nameserver list. 
    DNS Servers can be found here for reference https://support.cyberghostvpn.com/hc/en-us/articles/360012002360

--- run.sh
+++ run.sh
@@ -4,7 +4,20 @@
 		#Launch and connect to CyberGhost VPN [Example]
 		sudo cyberghostvpn --connect --country-code $COUNTRY --wireguard $ARGS
 		
-		#Add CyberGhost nameserver to resolv for DNS
-		echo 'nameserver 38.132.106.139' > /etc/resolv.conf
+		# Add CyberGhost nameserver to resolv for DNS
+		# This will switch baised on country selected
+		# https://support.cyberghostvpn.com/hc/en-us/articles/360012002360
+		case "$COUNTRY" in
+			"NL") echo 'nameserver 75.2.43.210' > /etc/resolv.conf
+			;;
+			"GB") echo 'nameserver 75.2.79.213' > /etc/resolv.conf
+			;;
+			"JP") echo 'nameserver 76.223.64.81' > /etc/resolv.conf
+			;;
+			"DE") echo 'nameserver 13.248.182.241' > /etc/resolv.conf
+			;;
+			*) echo 'nameserver 99.83.181.72' > /etc/resolv.conf # Default US
+			;;
+		esac
 	fi
 	
