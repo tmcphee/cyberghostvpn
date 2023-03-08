c47277fc1678dad42a2fb57ac30e4d5b8d90e414
Tyler McPhee <32398415+tmcphee@users.noreply.github.com>
2023-03-07 20:04:53 -0500

    Add options for customization though args

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 	config_ini=/home/root/.cyberghost/config.ini
 	if [ -f "$config_ini" ]; then
 		#Launch and connect to CyberGhost VPN [Example]
-		sudo cyberghostvpn --connect --torrent --country-code NL --wireguard
+		sudo cyberghostvpn --connect --country-code $COUNTRY --wireguard $ARGS
 		
 		#Add CyberGhost nameserver to resolv for DNS
 		echo 'nameserver 38.132.106.139' > /etc/resolv.conf
